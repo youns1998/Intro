665d6eb5222b04b7f11bdee5ada35c8b33a3a9ae
youns <youns1998@gmail.com>
2021-04-09 13:40:30 +0900

    first

---
+++ app/src/main/java/com/example/intro/WelcomeActivity.java
@@ -0,0 +1,44 @@
+package com.example.intro;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.intro.R;
+
+//오프닝 로딩 화면 액티비티
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_up);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(WelcomeActivity.this, BlankActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
