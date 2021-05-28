82256b2e2eee45e4d2f25c224bd19451152de000
SWLim238 <80829271+SWLim238@users.noreply.github.com>
2021-05-28 10:50:18 +0900

    design
    changer design

---
+++ freeidge-master/app/src/main/java/com/example/intro/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.intro;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.intro.menu.FragmentPage1;
+import com.example.intro.menu.FragmentPage2;
+import com.example.intro.menu.FragmentPage3;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView mBottomNV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //상태바 제거
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
+        setContentView(R.layout.activity_main);
+
+        //액션바 제거
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        mBottomNV = findViewById(R.id.nav_view);
+        mBottomNV.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() { //NavigationItemSelecte
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                BottomNavigate(menuItem.getItemId());
+
+
+                return true;
+            }
+        });
+        mBottomNV.setSelectedItemId(R.id.navigation_1);
+    }
+    private void BottomNavigate(int id) {  //BottomNavigation 페이지 변경
+        String tag = String.valueOf(id);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Fragment currentFragment = fragmentManager.getPrimaryNavigationFragment();
+        if (currentFragment != null) {
+            fragmentTransaction.hide(currentFragment);
+        }
+
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+        if (fragment == null) {
+            if (id == R.id.navigation_1) {
+                fragment = new FragmentPage1();
+
+            } else if (id == R.id.navigation_2){
+
+                fragment = new FragmentPage2();
+            }else {
+                fragment = new FragmentPage3();
+            }
+
+            fragmentTransaction.add(R.id.content_layout, fragment, tag);
+        } else {
+            fragmentTransaction.show(fragment);
+        }
+
+        fragmentTransaction.setPrimaryNavigationFragment(fragment);
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.commitNow();
+
+
+    }
+
+}
+
---
+++ freeidge-master/app/src/main/java/com/example/intro/database/TotalRecipe.java
@@ -0,0 +1,42 @@
+package com.example.intro.database;
+
+import java.util.ArrayList;
+
+public class TotalRecipe {
+    private String Title;
+    private String Image;
+    private String Ingredient;
+    private ArrayList<Context> context;
+
+    public ArrayList<Context> getContext() {
+        return context;
+    }
+
+    public void setContext(ArrayList<Context> context) {
+        this.context = context;
+    }
+
+    public String getIngredient() {
+        return Ingredient;
+    }
+    public void setIngredient(String ingredient) {
+        Ingredient = ingredient;
+    }
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
+
+
+}
